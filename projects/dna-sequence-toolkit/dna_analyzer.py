97bd92a2757d6d396e8fab8e3ce39034a61ea2f4
Surya <suryavaraprasadvegi.28.8.05@gmail.com>
2026-08-12 22:13:25 +0530

    add initial DNA sequence Analyzer

---
+++ projects/dna-sequence-toolkit/dna_analyzer.py
@@ -0,0 +1,9 @@
+# DNA Sequence Analyzer
+# First version: Basic sequence analyzer
+sequence = "ATGCGTGCTCAGCTAGCTAGCTAGCTTGGAACCCTTCCAAGG"
+print("DNA sequence:", sequence)
+print("length of sequence:", len(sequence))
+print("A:", sequence.count("A"))
+print("T:", sequence.count("T"))
+print("G:", sequence.count("G"))
+print("C:", sequence.count("C"))
